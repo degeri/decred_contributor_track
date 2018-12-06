f3a7f519be10a7f5f3ebed45f4f7f948c038c206
degeri <41839293+degeri@users.noreply.github.com>
2018-12-07 00:50:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,8 @@ if not db_file.is_file():
              (ID INTEGER PRIMARY KEY     AUTOINCREMENT,
              LOGIN           TEXT    NOT NULL,
              REPO           TEXT    NOT NULL,
-             FIRSTSEEN           DATE    NOT NULL)''')
+             FIRSTSEEN           DATE    NOT NULL,
+             ISFIRSTCONTRIBUTION	TEXT NOT NULL DEFAULT 0)''')
     conn.close()
 else:
     first_time = False
@@ -34,13 +35,20 @@ for repo in list_of_repos:
     contributors = (get_all_contributors(username, repo,token))
     for c in contributors:
         cursor.execute('''SELECT ID FROM contributors WHERE LOGIN=? AND REPO=?''', (c,repo))
+        conn.commit()
         if not cursor.fetchone():
+            is_first_contribution_value = 0
             if first_time:
                 date_value = "UNKNOWN"
             else:
+                cursor.execute('''SELECT ID FROM contributors WHERE LOGIN=?''', (c,))
+                conn.commit()
+                if not cursor.fetchone():
+                    is_first_contribution_value = 1
                 date_value = datetime.utcnow().strftime("%Y-%m-%d %H:%M")
-            cursor.execute('''INSERT INTO contributors(LOGIN, REPO, FIRSTSEEN)
-                          VALUES(?,?,?)''', (c,repo,date_value))
+            cursor.execute('''INSERT INTO contributors(LOGIN, REPO, FIRSTSEEN, ISFIRSTCONTRIBUTION)
+                          VALUES(?,?,?,?)''', (c,repo,date_value,is_first_contribution_value))
+            conn.commit()
 
 conn.commit()
 conn.close()
